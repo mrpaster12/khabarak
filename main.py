a4553df4bdb173e0d3ab5ed45a3b975f6180fa6c
mrpaster12 <iliataheri58@gmail.com>
2026-07-17 21:26:31 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
 import os
 import requests
 
-def ask_ai(text):
-    response = requests.post(
-        "https://openrouter.ai/api/v1/chat/completions",
-        headers={
-            "Authorization": f"Bearer {os.getenv('OPENROUTER_API_KEY')}",
-            "Content-Type": "application/json"
-        },
-        json={
-            "model": "deepseek/deepseek-chat-v3.1:free",
-            "messages": [
-                {
-                    "role": "user",
-                    "content": text
-                }
-            ]
-        }
-    )
-
-    return response.json()["choices"][0]["message"]["content"]
-summary = ask_ai(
-    "در یک جمله کوتاه به فارسی بگو سلام از OpenRouter"
+response = requests.post(
+    "https://openrouter.ai/api/v1/chat/completions",
+    headers={
+        "Authorization": f"Bearer {os.getenv('OPENROUTER_API_KEY')}",
+        "Content-Type": "application/json",
+    },
+    json={
+        "model": "openrouter/free",
+        "messages": [
+            {
+                "role": "user",
+                "content": "سلام"
+            }
+        ]
+    }
 )
 
-message = summary
+print(response.status_code)
+print(response.text)
