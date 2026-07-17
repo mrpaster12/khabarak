6b22ba3f2c628b7683084590ab6ad239bd56b796
mrpaster12 <iliataheri58@gmail.com>
2026-07-17 22:21:50 +0330

    Update telegram.py

--- publishers/telegram.py
+++ publishers/telegram.py
@@ -1,1 +1,17 @@
+import os
+import requests
 
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+def send_message(text):
+
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    requests.post(
+        url,
+        json={
+            "chat_id": CHAT_ID,
+            "text": text
+        }
+    )
