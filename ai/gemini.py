52fc0c09a40b8d1f89ec9a3a8db6bf542d8599ca
mrpaster12 <iliataheri58@gmail.com>
2026-07-17 22:15:55 +0330

    Update gemini.py

--- ai/gemini.py
+++ ai/gemini.py
@@ -1,1 +1,38 @@
+from google import genai
+import os
 
+client = genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY_1")
+)
+
+def summarize_news(title, summary):
+
+    prompt = f"""
+تو یک سردبیر خبری حرفه‌ای هستی.
+
+خبر زیر را بررسی کن.
+
+عنوان:
+{title}
+
+متن:
+{summary}
+
+اگر خبر مهم نیست فقط بنویس:
+
+SKIP
+
+اگر مهم است خروجی را به این شکل بده:
+
+اهمیت: X/10
+
+خلاصه:
+(حداکثر ۳ جمله فارسی)
+"""
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
+
+    return response.text
