b7d661f00ecec46a671d80d6a9c5b1545942fe9c
mrpaster12 <iliataheri58@gmail.com>
2026-07-17 22:21:34 +0330

    Update gemini.py

--- ai/gemini.py
+++ ai/gemini.py
@@ -5,45 +5,38 @@ client = genai.Client(
     api_key=os.getenv("GEMINI_API_KEY_1")
 )
 
-def summarize_news(title, summary):
-
-    prompt = f"""
+PROMPT = """
 تو یک سردبیر خبری حرفه‌ای هستی.
 
-خبر زیر را بررسی کن.
+قوانین:
+
+1- فقط خبرهای مهم و نیمه مهم را منتشر کن.
+2- خبرهای محلی، تبلیغاتی، ورزشی عادی و کم اهمیت را رد کن.
+3- اگر خبر مهم نیست فقط بنویس:
+
+SKIP
+
+4- اگر خبر مهم است خروجی دقیقاً به این شکل باشد:
+
+IMPORTANCE: عدد بین 1 تا 10
+
+SUMMARY:
+خلاصه فارسی در حداکثر 3 جمله
+"""
+
+def summarize_news(title, summary):
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=f"""
+{PROMPT}
 
 عنوان:
 {title}
 
 متن:
 {summary}
-
-اگر خبر مهم نیست فقط بنویس:
-
-SKIP
-
-اگر مهم است خروجی را به این شکل بده:
-
-اهمیت: X/10
-
-خلاصه:
-(حداکثر ۳ جمله فارسی)
 """
-
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=prompt
     )
 
     return response.text
-
-news = fetch_news()
-
-first_news = news[0]
-
-print(
-    summarize_news(
-        first_news["title"],
-        first_news["summary"]
-    )
-)
