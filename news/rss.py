b70a2681c2314b36c8914db5d9bfb7dafa612aaa
mrpaster12 <iliataheri58@gmail.com>
2026-07-17 22:11:18 +0330

    Update rss.py

--- news/rss.py
+++ news/rss.py
@@ -1,1 +1,15 @@
+RSS_FEEDS = [
+    # فارسی
+    "https://www.bbc.com/persian/index.xml",
+    "https://www.radiofarda.com/api/zrqiteuuvi",
 
+    # داخلی
+    "https://www.isna.ir/rss",
+    "https://www.irna.ir/rss",
+    "https://www.mehrnews.com/rss",
+
+    # انگلیسی
+    "https://rss.cnn.com/rss/edition.rss",
+    "https://rss.dw.com/rdf/rss-en-all",
+    "https://www.aljazeera.com/xml/rss/all.xml",
+]
