f7b7d2e58c5be634f63952b4b73136dce2240a4f
mrpaster12 <iliataheri58@gmail.com>
2026-07-17 21:17:00 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import os
+import requests
 
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+message = "🚀 ربات خبری با موفقیت اجرا شد."
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+requests.post(
+    url,
+    json={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
