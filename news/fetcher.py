9a4ee78f916a8f6a2d7457bacd70f534ac87c1d3
mrpaster12 <iliataheri58@gmail.com>
2026-07-18 10:00:23 +0330

    Update fetcher.py

--- news/fetcher.py
+++ news/fetcher.py
@@ -1,29 +1,25 @@
 import feedparser
 from news.rss_sources import RSS_FEEDS
 
-def fetch_news():
 
+def fetch_news():
     news = []
 
     for url in RSS_FEEDS:
-
         try:
-
             feed = feedparser.parse(url)
 
             source = feed.feed.get("title", url)
 
             for entry in feed.entries[:20]:
-
                 news.append({
                     "title": entry.get("title", ""),
                     "summary": entry.get("summary", ""),
                     "link": entry.get("link", ""),
-                    "source": source,
+                    "source": source
                 })
 
         except Exception as e:
-
-            print(f"ERROR {url}: {e}")
+            print(e)
 
     return news
